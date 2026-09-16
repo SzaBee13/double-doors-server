5081ec85351f958e2246ba0a202bf49ef818d685
Szabolcs Győrffy <me@szabee.me>
2026-09-16 19:57:21 +0200

    Remove unused assertTrue import

--- bukkit/src/test/java/me/szabee/doubledoors/bukkit/util/ProtectionCompatAdditionalTest.java
+++ bukkit/src/test/java/me/szabee/doubledoors/bukkit/util/ProtectionCompatAdditionalTest.java
@@ -2,7 +2,6 @@ package me.szabee.doubledoors.bukkit.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
